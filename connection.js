da67e963ffc0d66cefda058cfa38afb33e8bedbe
CarlinoCalogero <calogero.carlino@student.univaq.it>
2023-09-14 00:09:13 +0200

    working on adding menu items to database via menu.json file

--- connection.js
+++ connection.js
@@ -1,5 +1,10 @@
 const sqlite3 = require("sqlite3").verbose();
 const bcrypt = require('bcrypt');
+const fs = require('fs');
+
+var menu = JSON.parse(fs.readFileSync('./src/content/menu.json'));
+
+console.log(menu);
 
 // Connecting to or creating a new SQLite database file
 const db = new sqlite3.Database(
@@ -26,6 +31,7 @@ var semaphore = 0;
     // Serialize method ensures that database queries are executed sequentially
     db.serialize(() => {
 
+        //****** UTENTI ******//
         db.run(`drop table if exists utenti`);
         // Create utenti table if it doesn't exist
         db.run(
@@ -121,6 +127,163 @@ var semaphore = 0;
                 });
             }
         );
+
+        //****** CATEGORIA ******//
+        db.run(`drop table if exists categoria`);
+        // Create tables table if it doesn't exist
+        db.run(
+            `CREATE TABLE categoria (
+                nome VARCHAR(50),
+                CONSTRAINT nome_categoria PRIMARY KEY (nome)
+            )`);
+
+        //****** INGREDIENTE ******//
+        db.run(`drop table if exists ingrediente`);
+        // Create tables table if it doesn't exist
+        db.run(
+            `CREATE TABLE ingrediente (
+                nome VARCHAR(50) NOT NULL,
+                CONSTRAINT unique_ingrediente UNIQUE (nome)
+            )`);
+
+        //****** UNITA' DI MISURA ******//
+        db.run(`drop table if exists unita_di_misura`);
+        // Create tables table if it doesn't exist
+        db.run(
+            `CREATE TABLE unita_di_misura (
+                nome VARCHAR(50),
+                CONSTRAINT nome_unita_di_misura PRIMARY KEY (nome)
+            )`);
+
+        //****** MENU_ITEM ******//
+        db.run(`drop table if exists menu_item`);
+        // Create tables table if it doesn't exist
+        db.run(
+            `CREATE TABLE menu_item (
+                nome varchar(50) NOT NULL,
+                prezzo FLOAT NOT NULL,
+                nome_categoria VARCHAR(50) NOT NULL,
+                CONSTRAINT unique_menu_item UNIQUE (nome),
+                CONSTRAINT menu_item_categoria FOREIGN KEY (nome_categoria) REFERENCES categoria (nome) ON DELETE RESTRICT ON UPDATE CASCADE
+            )`,
+            (err) => {
+                if (err) {
+                    return console.error(err.message);
+                }
+
+                db.serialize(() => {
+
+                    //****** COMPONE ******//
+                    db.run(`drop table if exists compone`);
+                    // Create tables table if it doesn't exist
+                    db.run(
+                        `CREATE TABLE compone (
+                            id_ingrediente INTEGER UNSIGNED NOT NULL,
+                            id_menu_item INTEGER UNSIGNED NOT NULL,
+                            quantita INTEGER,
+                            nome_unita_di_misura VARCHAR(50),
+                            CONSTRAINT primary_key_compone PRIMARY KEY (id_ingrediente, id_menu_item),
+                            CONSTRAINT compone_ingrediente FOREIGN KEY (id_ingrediente) REFERENCES ingrediente (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+                            CONSTRAINT compone_menu_item FOREIGN KEY (id_menu_item) REFERENCES menu_item (id) ON DELETE CASCADE ON UPDATE CASCADE,
+                            CONSTRAINT quantita_unita_di_misura FOREIGN KEY (nome_unita_di_misura) REFERENCES unita_di_misura (nome) ON DELETE RESTRICT ON UPDATE CASCADE
+                        )`,
+                        (err) => {
+                            if (err) {
+                                return console.error(err.message);
+                            }
+
+                            db.serialize(() => {
+
+                                //****** ORDINAZIONE ******//
+                                db.run(`drop table if exists ordinazione`);
+                                // Create tables table if it doesn't exist
+                                db.run(
+                                    `CREATE TABLE ordinazione (
+                                        numero_tavolo INTEGER UNSIGNED NOT NULL,
+                                        data_e_ora DATETIME NOT NULL,
+                                        note varchar(250),
+                                        is_si_dividono_le_pizze BOOLEAN NOT NULL,
+                                        numero_ordinazione_progressivo_giornaliero INTEGER UNSIGNED NOT NULL,
+                                        pizze_divise_in INTEGER UNSIGNED,
+                                        numero_bambini INTEGER UNSIGNED,
+                                        numero_adulti INTEGER UNSIGNED NOT NULL,
+                                        CONSTRAINT unique_ordinazione UNIQUE (numero_tavolo , data_e_ora),
+                                        CHECK (pizze_divise_in IS NULL OR pizze_divise_in = 4 OR pizze_divise_in = 6 OR pizze_divise_in = 8)
+                                    )`,
+                                    (err) => {
+                                        if (err) {
+                                            return console.error(err.message);
+                                        }
+
+                                        db.serialize(() => {
+
+                                            //****** CONTIENE ******//
+                                            db.run(`drop table if exists contiene`);
+                                            // Create tables table if it doesn't exist
+                                            db.run(
+                                                `CREATE TABLE contiene (
+                                                    id_ordinazione INTEGER UNSIGNED NOT NULL,
+                                                    id_menu_item INTEGER UNSIGNED NOT NULL,
+                                                    quantita INTEGER NOT NULL,
+                                                    nome_unita_di_misura VARCHAR(50) NOT NULL,
+                                                    consegnato BOOLEAN NOT NULL,
+                                                    CONSTRAINT unique_contiene UNIQUE (id_ordinazione , id_menu_item),
+                                                    CONSTRAINT contiene_ordinazione FOREIGN KEY (id_ordinazione)
+                                                        REFERENCES ordinazione (id)
+                                                        ON DELETE CASCADE ON UPDATE CASCADE,
+                                                    CONSTRAINT contiene_menu_item FOREIGN KEY (id_menu_item)
+                                                        REFERENCES menu_item (id)
+                                                        ON DELETE RESTRICT ON UPDATE CASCADE,
+                                                    CONSTRAINT contiene_unita_di_misura FOREIGN KEY (nome_unita_di_misura)
+                                                        REFERENCES unita_di_misura (nome)
+                                                        ON DELETE RESTRICT ON UPDATE CASCADE
+                                                )`,
+                                                (err) => {
+                                                    if (err) {
+                                                        return console.error(err.message);
+                                                    }
+
+                                                    db.serialize(() => {
+
+                                                        //****** INTOLLERANZA ******//
+                                                        db.run(`drop table if exists intolleranza`);
+                                                        // Create tables table if it doesn't exist
+                                                        db.run(
+                                                            `CREATE TABLE intolleranza (
+                                                                id_contiene INTEGER UNSIGNED NOT NULL,
+                                                                id_ingrediente INTEGER UNSIGNED NOT NULL,
+                                                                CONSTRAINT primary_key_intolleranza PRIMARY KEY (id_contiene, id_ingrediente),
+                                                                CONSTRAINT intolleranza_contiene FOREIGN KEY (id_contiene) REFERENCES contiene (id) ON DELETE CASCADE ON UPDATE CASCADE,
+                                                                CONSTRAINT intolleranza_ingrediente FOREIGN KEY (id_ingrediente) REFERENCES ingrediente (id) ON DELETE RESTRICT ON UPDATE CASCADE
+                                                            )`,
+                                                            (err) => {
+                                                                if (err) {
+                                                                    return console.error(err.message);
+                                                                }
+
+                                                                //add menu here
+                                                                menu.forEach(menuEntry => {
+                                                                    insertCategoriaIntoDataBase(menuEntry);
+                                                                });
+                                                            }
+                                                        );
+
+                                                    })
+                                                }
+                                            );
+
+                                        })
+                                    }
+                                );
+
+                            })
+                        }
+                    );
+
+                })
+            }
+        );
+
     });
 
 })()
@@ -130,7 +293,7 @@ function checkIfFinished() {
         console.log("waiting");
     } else {
         console.log("wait finished");
-        closeDb();
+        //closeDb();
     }
 }
 
@@ -162,4 +325,60 @@ function insertIntoDatabase(insertSql, value) {
         semaphore++;
         checkIfFinished();
     });
+}
+
+function insertCategoriaIntoDataBase(menuEntry) {
+
+    var categoryName = menuEntry.categoryName;
+
+    db.run("INSERT INTO categoria(nome) VALUES (?)", categoryName, function (err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        const id = this.lastID; // get the id of the last inserted row
+        console.log(`Rows inserted, ID ${id}`);
+
+        if (menuEntry.entries != null) {
+
+            menuEntry.entries.forEach(categoryEntry => {
+                insertCategoryEntryIntoDataBase(categoryName, categoryEntry);
+            });
+
+        }
+
+    });
+
+}
+
+function insertCategoryEntryIntoDataBase(categoryName, categoryEntry) {
+
+    if (categoryEntry.subEntries == null && categoryEntry.price != null) {
+
+        insertMenuEntryIntoDataBase(categoryEntry.name, categoryEntry.price, categoryName)
+
+    } else {
+
+        categoryEntry.subEntries.forEach(categorySubEntry => {
+            insertMenuEntryIntoDataBase(categoryEntry.name + " " + categorySubEntry.name, categorySubEntry.price, categoryName)
+        });
+    }
+
+
+
+}
+
+function insertMenuEntryIntoDataBase(categoryEntryName, categoryEntryPrice, categoryName) {
+
+    console.log([categoryEntryName, Number(categoryEntryPrice.replace("€", "")), categoryName]);
+
+    db.run("INSERT INTO menu_item(nome, prezzo, nome_categoria) VALUES (?, ?, ?)", [categoryEntryName, Number(categoryEntryPrice.replace("€", "")), categoryName], function (err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        const id = this.lastID; // get the id of the last inserted row
+        console.log(`Rows inserted, ID ${id}`);
+
+    });
+
+
 }
